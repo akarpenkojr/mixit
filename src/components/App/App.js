ee73b958728a8e7e32303191b9b79ce743afca74
akarpenko_jr <andy.karpenko737@gmail.com>
2020-08-19 13:55:29 +0300

    Added new root component

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+/**
+ Created on 19.08.2020
+ */
+
+import React, {Component} from 'react'
+
+class App extends Component {
+   render() {
+      return (
+         <div>Hello world!</div>
+      )
+   }
+}
+
+export default App
---
+++ src/components/App/index.js
@@ -0,0 +1,5 @@
+/**
+ Created on 19.08.2020
+ */
+
+export {default} from './App'
