6b4fe507bced00ea513c0dc70fed5d9cf2242f33
akarpenko_jr <andy.karpenko737@gmail.com>
2020-08-20 14:18:09 +0300

    Test commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,45 @@
  Created on 19.08.2020
  */
 
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import './App.scss';
+import Fab from '@material-ui/core/Button';
+import AddIcon from '@material-ui/core/Icon';
 
 class App extends Component {
+   state = {
+      count: 0
+   };
+
+   componentDidMount() {
+      console.log('___---_-_!!!_-_---___', 'MOUNTED');
+   }
+
+   componentDidUpdate(prevProps, prevState, snapshot) {
+      console.log('___---_-_!!!_-_---___', 'UPDATED');
+   }
+
+   // constructor(props) {
+   //    //    super(props);
+   //    //
+   //    //    this.state = {
+   //    //       count: 0
+   //    //    }
+   //    // }
+
+
    render() {
       return (
-         <div>Hello world!</div>
+         <div>
+            <h1>Hello woasdasdrld!</h1>
+            <h2>{this.state.count}</h2>
+            <Fab
+                color="primary"
+                aria-label="add"
+            >
+               <AddIcon />
+            </Fab>
+         </div>
       )
    }
 }
