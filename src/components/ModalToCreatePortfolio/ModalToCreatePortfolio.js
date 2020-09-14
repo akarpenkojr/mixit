d2c0f4c557494474d697cd7999c136ad6a3653b8
akarpenko_jr <andy.karpenko737@gmail.com>
2020-09-14 17:14:56 +0300

    Added modal for creating a portfolio

---
+++ src/components/ModalToCreatePortfolio/ModalToCreatePortfolio.js
@@ -0,0 +1,22 @@
+/**
+ Created on 12.09.2020
+ */
+
+import React, {Component} from 'react'
+
+// import styles
+import styles from './ModalToCreatePortfolio.module.scss'
+
+class ModalToCreatePortfolio extends Component {
+   render() {
+      return (
+         <div>
+
+         </div>
+      )
+   }
+}
+
+export default ModalToCreatePortfolio
+
+
---
+++ src/components/ModalToCreatePortfolio/index.js
@@ -0,0 +1,5 @@
+/**
+ Created on 12.09.2020
+ */
+
+import ModalToCreatePortfolio from "./ModalToCreatePortfolio";
