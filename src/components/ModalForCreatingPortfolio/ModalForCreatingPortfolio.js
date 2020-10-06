245612fc6462bff980186e65f9733dd80b3bab7f
akarpenko_jr <andy.karpenko737@gmail.com>
2020-10-06 22:28:22 +0300

    Added some elements to modal form
    Added title of modal. Added inputs area for portfolio name and tax amount.
    Also changed styles and title of exit button.

--- src/components/ModalForCreatingPortfolio/ModalForCreatingPortfolio.js
+++ src/components/ModalForCreatingPortfolio/ModalForCreatingPortfolio.js
@@ -10,46 +10,51 @@ import React, {Component} from 'react'
 import styles from './ModalForCreatingPortfolio.module.scss'
 
 class ModalForCreatingPortfolio extends Component {
-  constructor(props) {
-    super(props);
+	state = {
+		newPortfolioModalIsOpen: this.props.newPortfolioModalIsOpen
+	};
 
-  }
+	closeModal (event)  {
+		if (event.target === event.currentTarget) {
+			this.props.updateData(false)
+		}
+	};
 
-   state = {
-     newPortfolioModalIsOpen: this.props.newPortfolioModalIsOpen
-   };
+	componentDidMount() {
 
-   closeModal (event)  {
-     console.log('___---_-_!!!_-_---___', event.target);
-     if (event.target === event.currentTarget) {
-       this.props.updateData(false)
-     }
-   };
+	}
 
-  componentDidMount() {
-
-  }
-
-   render() {
-
-      if (this.props.newPortfolioModalIsOpen) {
-         return (
-             <div className={styles.newPortfolioModal_wrapper} onClick={(event) => this.closeModal(event)}>
-               <div className={styles.newPortfolioModal_main}>
-                 <div className={styles.outer}>
-                   <div className={styles.inner} onClick={(event) => this.closeModal(event)}>
-                     <label onClick={(event) => this.closeModal(event)}>Close</label>
-                   </div>
-                 </div>
-               </div>
-             </div>
-         )
-      }
-      return (
-          <div>
-          </div>
-      )
-   }
+	render() {
+		if (this.props.newPortfolioModalIsOpen) {
+			return (
+					<div className={styles.newPortfolioModal_wrapper} onClick={(event) => this.closeModal(event)}>
+						<div className={styles.newPortfolioModal_mainBlock}>
+							<div className={styles.outer}>
+								<div className={styles.inner} onClick={(event) => this.closeModal(event)}>
+									<label onClick={(event) => this.closeModal(event)}>Закрыть</label>
+								</div>
+							</div>
+							<div className={styles.newPortfolioModal_title}>
+								Название портфеля
+							</div>
+							<div className={styles.newPortfolioModal_dataArea}>
+								<div className={styles.newPortfolioModal_inputArea}>
+									<div className={styles.newPortfolioModal_dataAreaTitle}>
+										Данные нового портфеля
+									</div>
+									<input type="text" className={styles.newPortfolioModal_input} required/>
+									<input type="text" className={styles.newPortfolioModal_input} required/>
+								</div>
+							</div>
+						</div>
+					</div>
+			)
+		}
+		return (
+				<div>
+				</div>
+		)
+	}
 }
 
 export default ModalForCreatingPortfolio
