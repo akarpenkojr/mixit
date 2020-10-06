0d1301c7a1d806e53b5aba011a057e2ff6d221bc
akarpenko_jr <andy.karpenko737@gmail.com>
2020-10-06 14:21:48 +0300

    Added exit icon/button
    Also added function for closing modal window for exit icon

--- src/components/ModalForCreatingPortfolio/ModalForCreatingPortfolio.js
+++ src/components/ModalForCreatingPortfolio/ModalForCreatingPortfolio.js
@@ -20,6 +20,7 @@ class ModalForCreatingPortfolio extends Component {
    };
 
    closeModal (event)  {
+     console.log('___---_-_!!!_-_---___', event.target);
      if (event.target === event.currentTarget) {
        this.props.updateData(false)
      }
@@ -33,14 +34,13 @@ class ModalForCreatingPortfolio extends Component {
 
       if (this.props.newPortfolioModalIsOpen) {
          return (
-             <div
-                 className={styles.newPortfolioModal_wrapper}
-                 onClick={(event) => this.closeModal(event)}
-             >
-               <div
-                   className={styles.newPortfolioModal_main}
-               >
-
+             <div className={styles.newPortfolioModal_wrapper} onClick={(event) => this.closeModal(event)}>
+               <div className={styles.newPortfolioModal_main}>
+                 <div className={styles.outer}>
+                   <div className={styles.inner} onClick={(event) => this.closeModal(event)}>
+                     <label onClick={(event) => this.closeModal(event)}>Close</label>
+                   </div>
+                 </div>
                </div>
              </div>
          )
