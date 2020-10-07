a20fa78af51d1217e660ae151299ff6d4bbc3bb4
akarpenko_jr <andy.karpenko737@gmail.com>
2020-10-07 22:28:41 +0300

    Added onChange action for input of portfolio name
    Added title to the modal and changed its styles for constant height
    Also deleted closing modal on click to wrapper of modal

--- src/components/ModalForCreatingPortfolio/ModalForCreatingPortfolio.js
+++ src/components/ModalForCreatingPortfolio/ModalForCreatingPortfolio.js
@@ -10,14 +10,30 @@ import React, {Component} from 'react'
 import styles from './ModalForCreatingPortfolio.module.scss'
 
 class ModalForCreatingPortfolio extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			newPortfolioModalIsOpen: this.props.newPortfolioModalIsOpen,
+			portfolioName: '',
+			portfolioTaxAmount: ''
+		};
+		this.enterPortfolioName = this.enterPortfolioName.bind(this);
+	}
+
 	state = {
-		newPortfolioModalIsOpen: this.props.newPortfolioModalIsOpen
+
 	};
 
 	closeModal (event)  {
 		if (event.target === event.currentTarget) {
-			this.props.updateData(false)
+			this.props.updateData(false);
+			this.setState({portfolioName: ''});
 		}
+	};
+
+	enterPortfolioName(event) {
+
 	};
 
 	componentDidMount() {
@@ -27,7 +43,7 @@ class ModalForCreatingPortfolio extends Component {
 	render() {
 		if (this.props.newPortfolioModalIsOpen) {
 			return (
-					<div className={styles.newPortfolioModal_wrapper} onClick={(event) => this.closeModal(event)}>
+					<div className={styles.newPortfolioModal_wrapper}>
 						<div className={styles.newPortfolioModal_mainBlock}>
 							<div className={styles.outer}>
 								<div className={styles.inner} onClick={(event) => this.closeModal(event)}>
@@ -35,15 +51,30 @@ class ModalForCreatingPortfolio extends Component {
 								</div>
 							</div>
 							<div className={styles.newPortfolioModal_title}>
-								Название портфеля
+								{this.state.portfolioName}
 							</div>
 							<div className={styles.newPortfolioModal_dataArea}>
 								<div className={styles.newPortfolioModal_inputArea}>
 									<div className={styles.newPortfolioModal_dataAreaTitle}>
 										Данные нового портфеля
 									</div>
-									<input type="text" className={styles.newPortfolioModal_input} required/>
-									<input type="text" className={styles.newPortfolioModal_input} required/>
+									<input
+											value={this.state.portfolioName}
+											onChange={event => {
+												this.setState({portfolioName: event.target.value});
+											}}
+											type="text"
+											className={styles.newPortfolioModal_input}
+											placeholder={'Введите имя портфеля'}
+											maxLength={27}
+											required
+									/>
+									<input
+											type="number"
+											className={styles.newPortfolioModal_input}
+											placeholder={'Введите размер комиссии портфеля'}
+											required
+									/>
 								</div>
 							</div>
 						</div>
