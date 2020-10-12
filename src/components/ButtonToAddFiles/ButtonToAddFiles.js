4f8794fc4007f59a395d7ab3ee6faef497aa8d2b
akarpenko_jr <andy.karpenko737@gmail.com>
2020-10-12 22:29:06 +0300

    Added functions for choosing and reading of file.
    First, choose file and than it read with FileReader. The data from the file is then modified from a string to an object.
    Also added test button for check data from received file. It shows in console some string from read object.

--- src/components/ButtonToAddFiles/ButtonToAddFiles.js
+++ src/components/ButtonToAddFiles/ButtonToAddFiles.js
@@ -6,8 +6,30 @@ import React from 'react'
 import styles from './ButtonToAddFiles.module.scss'
 
 function ButtonToAddFiles(props) {
+	let fileReader;
+
+	const handleFileRead = (e) => {
+		const content = fileReader.result;
+		console.log(content)
+		// … do something with the 'content' …
+	};
+
+	const handleFileChosen = (file) => {
+		fileReader = new FileReader();
+		fileReader.onloadend = handleFileRead;
+		fileReader.readAsText(file);
+	};
+
 	return (
-			<div>
+			<div >
+				<div
+						style={{backgroundColor: '#FFFFFF', margin: '10px'}}
+						onClick={() => {
+							let a = JSON.parse(fileReader.result);
+							console.log(a['appname']);
+				}}>
+					READ DATA FROM FILE
+				</div>
 				<label htmlFor={"upload-json"} className={styles.addButton} >Добавить файлы</label>
 				<input
 						className={styles['input-file']}
@@ -16,9 +38,7 @@ function ButtonToAddFiles(props) {
 						name="file"
 						multiple
 						accept={'.json'}
-						onChange={event => {
-							console.log(event.target.files[0].name)
-						}}
+						onChange={e => handleFileChosen(e.target.files[0])}
 				/>
 			</div>
 	)
